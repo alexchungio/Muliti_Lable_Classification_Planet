ec3b693f898ce813e8d1e9bd79364fa9639ebe80
alexchung <yonganzhong@outlook.com>
2020-12-04 10:05:16 +0800

    update confusion matrix

--- docs/confusion_matrix.py
+++ docs/confusion_matrix.py
@@ -16,81 +16,30 @@ from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import itertools
 
-def plot_confusion_matrix(cm,
-                          target_names,
-                          title='Confusion matrix',
-                          cmap=None,
-                          normalize=True):
-    """
-    given a sklearn confusion matrix (cm), make a nice plot
 
-    Arguments
-    ---------
-    cm:           confusion matrix from sklearn.metrics.confusion_matrix
-
-    target_names: given classification classes such as [0, 1, 2]
-                  the class names, for example: ['high', 'medium', 'low']
-
-    title:        the text to display at the top of the matrix
-
-    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
-                  see http://matplotlib.org/examples/color/colormaps_reference.html
-                  plt.get_cmap('jet') or plt.cm.Blues
-
-    normalize:    If False, plot the raw numbers
-                  If True, plot the proportions
-
-    Usage
-    -----
-    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
-                                                              # sklearn.metrics.confusion_matrix
-                          normalize    = True,                # show proportions
-                          target_names = y_labels_vals,       # list of names of the classes
-                          title        = best_estimator_name) # title of graph
-
-    Citiation
-    ---------
-    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
-
+def get_confusion_matrix(y_label, y_pred):
     """
 
-    accuracy = np.trace(cm) / float(np.sum(cm))
-    misclass = 1 - accuracy
+    :param y_label:
+    :param y_pred:
+    :return:
+    """
+    y_label = np.asarray(y_label, dtype=np.int32)
+    y_pred = np.array(y_pred, dtype=np.int32)
+    # get unique label
+    labels = np.asarray(sorted(set(y_label)))
+    # num classes
+    num_labels = labels.size
+    # label_to_ind = {y: x for x, y in enumerate(labels)}
+    # # # convert yt, yp into index
+    # y_pred = np.array([label_to_ind.get(x, num_labels + 1) for x in y_pred])
+    # y_label = np.array([label_to_ind.get(x, num_labels + 1) for x in y_label])
+    cm = np.zeros((num_labels, num_labels), dtype=np.int32)
 
-    if cmap is None:
-        cmap = plt.get_cmap('Blues')
+    for l_index, p_index in zip(y_label, y_pred):
+        cm[l_index, p_index] += 1
 
-    plt.figure(figsize=(8, 6))
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
-
-    if target_names is not None:
-        tick_marks = np.arange(len(target_names))
-        plt.xticks(tick_marks, target_names, rotation=45)
-        plt.yticks(tick_marks, target_names)
-
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-
-
-    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        if normalize:
-            plt.text(j, i, "{:0.4f}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-        else:
-            plt.text(j, i, "{:,}".format(cm[i, j]),
-                     horizontalalignment="center",
-                     color="white" if cm[i, j] > thresh else "black")
-
-
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label\naccuracy={:0.4f}; misclass={:0.4f}'.format(accuracy, misclass))
-    plt.show()
-
+    return cm
 
 def visual_confusion_matrix(cm, normalize=True, title='confusion matrix', ticklabel=None):
     """
@@ -108,11 +57,12 @@ def visual_confusion_matrix(cm, normalize=True, title='confusion matrix', tickla
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
-    if ticklabel is not None:
+    sns.heatmap(cm, annot=True, center=0, linewidths=.5)
 
-        sns.heatmap(cm, annot=True, center=0, xticklabels=ticklabel,  yticklabels=ticklabel, linewidths=.5)
-    else:
-        sns.heatmap(cm, annot=True, center=0, linewidths=.5)
+    if ticklabel is not None:
+        tick_marks = np.arange(len(ticklabel)) + 0.5
+        plt.xticks(tick_marks, ticklabel, rotation=45)
+        plt.yticks(tick_marks, ticklabel)
 
     plt.tight_layout()
     plt.ylabel('label')
@@ -122,13 +72,14 @@ def visual_confusion_matrix(cm, normalize=True, title='confusion matrix', tickla
 
 
 def main():
-    class_name = ['high', 'medium', 'low']
+    class_name = ['low', 'medium', 'high']
 
     y_label = [0, 1, 2, 1, 1, 0, 2, 1, 0, 2]
 
     y_pred =  [0, 1, 1, 2, 1, 0, 2, 0, 0, 2]
 
-    cm = confusion_matrix(y_label, y_pred)
+    # cm = confusion_matrix(y_label, y_pred)
+    cm = get_confusion_matrix(y_label, y_pred)
 
     visual_confusion_matrix(cm, ticklabel=class_name)
 
