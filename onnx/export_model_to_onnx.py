f96f390c433f2313afd8de51dd81fce750ce93c0
alexchung <yonganzhong@outlook.com>
2021-01-22 15:53:54 +0800

    add export_model_to_onnx

---
+++ onnx/export_model_to_onnx.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : export_model_to_onnx.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2021/1/22 下午1:38
+# @ Software   : PyCharm
+#-------------------------------------------------------
+
+
